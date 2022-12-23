89425dd68ef782cfec90917435700f1feb465776
n28div <nicolaslazzari98@gmail.com>
2022-12-23 11:10:02 +0100

    generate chords without temporary files

--- gen_data_from_json.py
+++ gen_data_from_json.py
@@ -13,33 +13,23 @@ if __name__ == "__main__":
   argument_parser.add_argument("--prefix", type=str, required=True)
   argument_parser.add_argument("--out", type=str, required=True)
   argument_parser.add_argument("--json", type=str, required=False)
+  argument_parser.add_argument("--format", type=str, required=False, default="turtle")
   argument_parser.add_argument("--progress", action='store_true')
   args = argument_parser.parse_args()
 
   with open(args.json) as f:
     chords = json.load(f)
 
+  graph = rdflib.Graph()
   
-  chords_dir = os.path.join(args.out, "chords")
-  if not os.path.exists(chords_dir):
-    os.mkdir(chords_dir)
-  
-  for idx, chord in tqdm.tqdm(enumerate(chords.keys()), disable=not args.progress):
+  for idx, chord in tqdm.tqdm(list(enumerate(chords.keys())), disable=not args.progress):
     try:
-      out_graph = convert_chord(chord, args.prefix)
-      out_file = os.path.join(chords_dir, f"chord_{idx}.ttl")
-      
-      if not os.path.exists(out_file):
-        out_graph.serialize(out_file)
+      tmp_graph = convert_chord(chord, args.prefix)
+      graph.parse(data=tmp_graph.serialize())
     except Exception as e:
       print("Error parsing ", chord)
-
-  # merge turtles
-  graph = rdflib.Graph()
-  for ttl in tqdm.tqdm(os.listdir(chords_dir), disable=not args.progress):
-    graph.parse(os.path.join(chords_dir, ttl))
   
-  graph.serialize(os.path.join(args.out, "chords.ttl"))
+  graph.serialize(args.out, format=turtle)
 
   
   
